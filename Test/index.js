5fd6f36384a4f5c6ad00ffd1d231aabc2137a96d
ssloth1 <jamesbebarski@gmail.com>
2024-08-07 20:10:17 -0400

    Update index.js

--- Test/index.js
+++ Test/index.js
@@ -1,4 +1,9 @@
 export default function Test(app) {
+
+	app.get('/', (req, res) => {
+		res.send('Welcome to Chatterbox!, please use /test to test the server')
+	});
+
 	app.get('/test', (req, res) => {
 		res.send('Welcome to Chatterbox!')
 	});
