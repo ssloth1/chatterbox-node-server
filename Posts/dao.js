4da886047e866b9ad068fb793e8a6915a53db9fc
Mansi142000 <155905751+Mansi142000@users.noreply.github.com>
2024-08-18 02:17:57 -0400

    changes for posts

--- Posts/dao.js
+++ Posts/dao.js
@@ -6,3 +6,14 @@ export const findAllPosts = () => model.find();
 export const findPostById = (postId) => model.findById(postId);
 export const updatePost = (postId, post) => model.updateOne({ _id: postId }, { $set: post });
 export const deletePost = (postId) => model.deleteOne({ _id: postId });
+export const findPostforTopic = (topicId, searchString) => {
+    const searchRegex = new RegExp(searchString, 'i');  
+    return model.find({
+      topic: topicId,
+      $or: [
+        { postTitle: { $regex: searchRegex } },
+        { postDesc: { $regex: searchRegex } }
+      ]
+    });
+  };
+  
--- Posts/routes.js
+++ Posts/routes.js
@@ -1,29 +1,42 @@
 // chatterbox-node-server/Posts/routes.js
-import * as dao from './dao.js';
+import * as dao from "./dao.js";
 
 export default function PostRoutes(app) {
-	app.post('/api/posts', async (req, res) => {
-		const post = await dao.createPost(req.body);
-		res.json(post);
-	});
+  app.post("/api/topics/:tid/post", async (req, res) => {
+    const { tid } = req.params;
+    const post = {
+      ...req.body,
+      topic: tid,
+      _id: new Date().getTime().toString(),
+    };
+    await createPost(post);
+    res.json(post);
+  });
 
-	app.get('/api/posts', async (req, res) => {
-		const posts = await dao.findAllPosts();
-		res.json(posts);
-	});
+  app.get("/api/topics/:tid/posts", async (req, res) => {
+    const { tid } = req.params;
+    const { search } = req.query;
+    try {
+      const posts = await findPostforTopic(tid, search);
+      res.json(posts);
+    } catch (error) {
+      console.error("Failed to fetch posts:", error);
+      res.status(500).json({ message: "Failed to fetch posts" });
+    }
+  });
 
-	app.get('/api/posts/:postId', async (req, res) => {
-		const post = await dao.findPostById(req.params.postId);
-		res.json(post);
-	});
+  app.get("/api/posts/:postId", async (req, res) => {
+    const post = await dao.findPostById(req.params.postId);
+    res.json(post);
+  });
 
-	app.put('/api/posts/:postId', async (req, res) => {
-		const status = await dao.updatePost(req.params.postId, req.body);
-		res.json(status);
-	});
+  app.put("/api/posts/:postId", async (req, res) => {
+    const status = await dao.updatePost(req.params.postId, req.body);
+    res.json(status);
+  });
 
-	app.delete('/api/posts/:postId', async (req, res) => {
-		const status = await dao.deletePost(req.params.postId);
-		res.json(status);
-	});
+  app.delete("/api/posts/:postId", async (req, res) => {
+    const status = await dao.deletePost(req.params.postId);
+    res.json(status);
+  });
 }
