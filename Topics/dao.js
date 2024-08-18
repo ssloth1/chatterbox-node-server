4b4164956d9c6ec13364286df1e4ab1b5496f5e5
Mansi142000 <155905751+Mansi142000@users.noreply.github.com>
2024-08-18 02:06:39 -0400

    changes

--- Topics/dao.js
+++ Topics/dao.js
@@ -3,7 +3,23 @@
 import model from "./model.js";
 
 export const createTopic = (topic) => model.create(topic);
-export const findAllTopics = () => model.find();
+export const findAllTopics = async (searchString = "") => {
+  try {
+    let query = {};
+    if (searchString) {
+      query.$or = [
+        { topicTitle: { $regex: searchString, $options: "i" } },
+        { topicDesc: { $regex: searchString, $options: "i" } },
+      ];
+    }
+    const topics = await model.find(query);
+    return topics;
+  } catch (error) {
+    console.error("Error fetching topics:", error);
+    throw error;
+  }
+};
 export const findTopicById = (topicId) => model.findById(topicId);
-export const updateTopic = (topicId, topic) => model.updateOne({ _id: topicId }, { $set: topic });
+export const updateTopic = (topicId, topic) =>
+  model.updateOne({ _id: topicId }, { $set: topic });
 export const deleteTopic = (topicId) => model.deleteOne({ _id: topicId });
--- Topics/routes.js
+++ Topics/routes.js
@@ -1,30 +1,35 @@
 // chatterbox-node-server/src/Topics/routes.js
 
-import * as dao from './dao.js';
+import * as dao from "./dao.js";
 
 export default function TopicRoutes(app) {
-	app.post('/api/topics', async (req, res) => {
-		const topic = await dao.createTopic(req.body);
-		res.json(topic);
-	});
+  app.post("/api/topics", async (req, res) => {
+    const topic = await dao.createTopic(req.body);
+    res.json(topic);
+  });
 
-	app.get('/api/topics', async (req, res) => {
-		const topics = await dao.findAllTopics();
-		res.json(topics);
-	});
+  app.get("/api/topics", async (req, res) => {
+    const searchText = req.query.searchText || "";
+    try {
+      const topics = await findAllTopics(searchText);
+      res.send(topics);
+    } catch (error) {
+      res.status(500).send({ error: "Failed to fetch topics" });
+    }
+  });
 
-	app.get('/api/topics/:topicId', async (req, res) => {
-		const topic = await dao.findTopicById(req.params.topicId);
-		res.json(topic);
-	});
+  app.get("/api/topics/:topicId", async (req, res) => {
+    const topic = await dao.findTopicById(req.params.topicId);
+    res.json(topic);
+  });
 
-	app.put('/api/topics/:topicId', async (req, res) => {
-		const status = await dao.updateTopic(req.params.topicId, req.body);
-		res.json(status);
-	});
+  app.put("/api/topics/:topicId", async (req, res) => {
+    const status = await dao.updateTopic(req.params.topicId, req.body);
+    res.json(status);
+  });
 
-	app.delete('/api/topics/:topicId', async (req, res) => {
-		const status = await dao.deleteTopic(req.params.topicId);
-		res.json(status);
-	});
+  app.delete("/api/topics/:topicId", async (req, res) => {
+    const status = await dao.deleteTopic(req.params.topicId);
+    res.json(status);
+  });
 }
