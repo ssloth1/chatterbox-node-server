11e3e06691fa39c801a206ccb45f9efe93ca7add
ssloth1 <jamesbebarski@gmail.com>
2024-08-18 01:32:45 -0400

    Update dao.js

--- Topics/dao.js
+++ Topics/dao.js
@@ -2,7 +2,7 @@
 
 import model from "./model.js";
 
-export const createTopic = (topic) => model.creater(topic);
+export const createTopic = (topic) => model.create(topic);
 export const findAllTopics = () => model.find();
 export const findTopicById = (topicId) => model.findById(topicId);
 export const updateTopic = (topicId, topic) => model.updateOne({ _id: topicId }, { $set: topic });
